c56d8c115766280580bac577d6b5773e9e2acfa3
Harsh 8867 <harsh.sharma280196@gmail.com>
2022-08-08 22:12:08 +0530

    new committed

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+function Header() {
+  return (
+    <div className="header-text">
+      <h1>🏍️ My travel journal</h1>
+    </div>
+  );
+}
+
+export default Header;
