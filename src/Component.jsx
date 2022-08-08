b34278c746be2727cb414615ebf0e1ef9dca2ead
Harsh 8867 <harsh.sharma280196@gmail.com>
2022-08-08 22:10:13 +0530

    new commit

---
+++ src/Component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Component() {
+  return (
+    <div>
+      <img src="" alt="" />
+      <p>Japan</p>
+      <a href="www.google.com">Google</a>
+      <h3>Mount Fuji</h3>
+      <p>something date</p>
+      <p>Description</p>
+    </div>
+  );
+}
