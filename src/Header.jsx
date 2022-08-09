575334f0b030e09990bcdc406f705f0a133417b1
Harsh 8867 <harsh.sharma280196@gmail.com>
2022-08-09 22:29:07 +0530

    buttton

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,6 +5,17 @@ function Header() {
   return (
     <div className="header-text">
       <h1>🏍️ My travel journal</h1>
+      <button className="btn-53">
+        <div class="original">Button</div>
+        <div class="letters">
+          <span>B</span>
+          <span>U</span>
+          <span>T</span>
+          <span>T</span>
+          <span>O</span>
+          <span>N</span>
+        </div>
+      </button>
     </div>
   );
 }
